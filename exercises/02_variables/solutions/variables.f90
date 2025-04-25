cb8301e45ec13fb73aa7f5d3c1d6f422eed71dc8
Dimitrios Theodorakis <astrodimitrios@gmail.com>
2025-04-25 16:00:12 +0100

    Tidy variables.f90 code

--- exercises/02_variables/solutions/variables.f90
+++ exercises/02_variables/solutions/variables.f90
@@ -4,27 +4,32 @@
 !--------------------------------------------------------
 !
 program variables
+    !! A test program to lean how to declare and assign variables.
 
     implicit none
 
-    integer   :: number_of_pelicans
-    real      :: pelican_weight
-    complex   :: pelican_population_dynamics
-    logical   :: is_young_pelican
+    integer :: number_of_pelicans
+      !! The number of pelicans in the pod
+    real    :: pelican_weight
+      !! The average weight of a pelican in the pod / kg
+    complex :: pelican_population_dynamics
+      !! The birth and death rate as a complex number
+      !! Units are the number of pelicans per year
+    logical :: is_young_pelican
+      !! Test to see if the current pelican is young
+
     character, parameter :: pelican_tag = 'J'
+      !! Pelican pod tracking tag
 
     number_of_pelicans          = 5
-    pelican_weight              = 2.5  ! kg
-    pelican_population_dynamics = (-1.2, 0.9)
+    pelican_weight              = 2.5          ! kg
+    pelican_population_dynamics = (-1.2, 0.9)  ! births, deaths per year
     is_young_pelican            = .false.
-    ! pelican_tag               = 'J'
 
     print *, number_of_pelicans
     print *, pelican_weight
     print *, pelican_population_dynamics
     print *, is_young_pelican
     print *, pelican_tag
-
-    pelican_tag = 'F'
 
 end program variables
