4ab1cf064393c2bcd95a106d29f9a18c56340556
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2026-04-30 14:02:50 +0100

    Add docstrings

--- episodes/files/utils.R
+++ episodes/files/utils.R
@@ -1,24 +1,52 @@
+#' Render a link containing a fortran program
+#' 
+#' @param program the program as a single string
+#' @param link_stub the base of the link that the program will be inserted to
+#' @param link_text the text to be displayed as the link
+#' @return A markdown-formatted link string.
 render_link <- function(program, link_stub, link_text) {
-    paste0("[", link_text, "](", link_stub, utils::URLencode(program), ")")
+    paste0("You can run this program on [", link_text, "](", link_stub, utils::URLencode(program), ").")
 }
 
+#' Render a fortran program as a fenced block
+#' 
+#' @param program the program as a single string
+#' @return A markdown-formatted fortran fence block
 render_block <- function(program) {
     paste(c("```fortran", program, "```"), collapse="\n")
 }
 
-cat_link_and_block <- function(program, link_stub="https://dev.lfortran.org/?code=", link_text="Link to run the program") {
-    cat(render_block(program))
-    cat("\n")
+#' Render a fortran program as a fenced block
+#' 
+#' @param program the program as a single string
+#' @param link_stub the base of the link that the program will be inserted to
+#' @param link_text the text to be displayed as the link
+#' @return Nothing, but it does cat the link and then the program as a fenced block  
+cat_link_and_block <- function(program, link_stub="https://dev.lfortran.org/?code=", link_text="lfortran") {
     cat(render_link(program, link_stub, link_text))
+    cat("\n")
+    cat(render_block(program))
     cat("\n")
 }
 
-
-read_program <- function(program_name, program_dir="") {
+#' Read a program from a file
+#' 
+#' @param program_dir the directory containing the program file
+#' @param program_name the program name (including file extension)
+#' @return The program as a single string with newlines
+read_program <- function(program_dir="./", program_name) {
     paste(readLines(paste(program_dir, program_name, sep="/")), collapse="\n")
 }
 
-cat_program <- function(program_dir, program_name, link_stub="https://dev.lfortran.org/?code=", link_text="Link to run the program") {
+#' Read a program from a file
+#' 
+#' @param program_dir the directory containing the program file
+#' @param program_name the program name (including file extension)
+#' @param link_stub the base of the link that the program will be inserted to
+#' @param link_text the text to be displayed as the link
+#'
+#' @return The program as a single string with newlines
+cat_program <- function(program_dir, program_name, link_stub="https://dev.lfortran.org/?code=", link_text="lfortran") {
     program <- read_program(program_name=program_name, program_dir=program_dir)
     cat_link_and_block(program, link_stub=link_stub, link_text=link_text)
 }
